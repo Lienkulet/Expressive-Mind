e1574f69ceb68f9bcb3cf110371a249a0d521456
Lienkulet <clocicovalexandru@gmail.com>
2023-05-28 14:29:13 +0300

    vercel link

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { CircleLoader } from 'react-spinners';
 
 export  async function fetchBlogs(){
-  const res = await fetch('http://localhost:3000/api/blog', {
+  const res = await fetch('https://expressive-mind-6470j9tbe-lienkulet.vercel.app/api/blog', {
     method: 'GET',  
     cache: 'no-store'
   });
